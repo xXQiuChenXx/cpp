a3855aff816eaf7e64d82e75835d1696616dc6ce
xXQiuChenXx <lautaihong2004@gmail.com>
2024-01-25 07:35:54 +0800

    exercise

---
+++ Chapter 3/exercise.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    int first, second, sum;
+    cout << "Enter first integer" << endl;
+    cin >> first;
+    cout << "Enter second integer" << endl;
+    cin >> second;
+    sum = first + second;
+    cout << "Sum is " << sum << endl;
+
+    return 0;
+}
